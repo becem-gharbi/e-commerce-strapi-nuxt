d3139fe6d13fffc1f48a5102847469a47ef00986
becem-gharbi <becem.gharbi@live.com>
2022-06-28 22:35:56 +0100

    .

--- backend/config/env/production/middlewares.js
+++ backend/config/env/production/middlewares.js
@@ -1,24 +1,37 @@
+// module.exports = [
+//     'strapi::errors',
+//     {
+//       name: 'strapi::security',
+//       config: {
+//         contentSecurityPolicy: {
+//           useDefaults: true,
+//           directives: {
+//             'connect-src': ["'self'", 'https:'],
+//             'img-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
+//             'media-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
+//             upgradeInsecureRequests: null,
+//           },
+//         },
+//       },
+//     },
+//     'strapi::cors',
+//     'strapi::poweredBy',
+//     'strapi::logger',
+//     'strapi::query',
+//     'strapi::body',
+//     'strapi::favicon',
+//     'strapi::public',
+//   ];
+
 module.exports = [
-    'strapi::errors',
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
-            'media-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
-            upgradeInsecureRequests: null,
-          },
-        },
-      },
-    },
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::favicon',
-    'strapi::public',
-  ];
+  'strapi::errors',
+  'strapi::security',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  'strapi::cors',
+];
